c44370fe58025b577b6c5f0dda7af9cbc3e71fb3
Roberto Jacquez <rjqz92@gmail.com>
2023-05-01 22:19:36 -0600

    Created user model

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://i.gifer.com/J2uL.gif" className="App-logo" alt="Togepi" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Models: User,
+          Pokemon,
+          Pokemon_Type (joins table which has pokemon_id and element_id), Element 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+       
       </header>
     </div>
   );
